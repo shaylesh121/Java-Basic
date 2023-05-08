7c1e6584e26e1281ab688766d40fe62cd6164cc6
Shaylesh Maharjan <mshylesh34@gmail.com>
2023-05-08 22:03:14 +0545

    Inheritance

---
+++ Inheritance/Family.java
@@ -0,0 +1,45 @@
+package family;
+
+class Grandfather {
+     int cash1 = 10;
+     int asset1 = 20;
+}
+
+class Father extends Grandfather {
+     int cash2 = 5;
+     int asset2 = 22;
+    
+    public void calculateProperties() {
+        cash2 += cash1;
+        asset2 += asset1;
+    }
+}
+
+class You extends Father {
+     int cash=22;
+     int asset=0;
+     int total = 0;
+    
+    
+    public void calculateTotalProperties() {
+        cash = cash2 - cash;
+        asset=asset2;
+        
+        if (cash < 0) {
+            total = asset + cash;
+        } else {
+            total = cash;
+        }
+        
+        System.out.print("Total Properties = " + total);
+        System.out.print(" lakh");
+    }
+}
+
+public class Family {
+    public static void main(String[] args) {
+        You object = new You();
+        object.calculateProperties();
+        object.calculateTotalProperties();
+    }
+}
